327d12cbcccd1a1e01b163c010519236f0f33fde
joshuaiokua <jiokua.albano@gmail.com>
2024-09-25 02:23:46 -0700

    fix: __dirname and export default
    Fixed issues that required explicit definition of directory name and module default export

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,25 +1,32 @@
-import { resolve as _resolve, join } from 'path';
+import { resolve as _resolve, join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-export const entry = './src/index.tsx';
-export const output = {
-  filename: 'bundle.js',
-  path: _resolve(__dirname, 'dist')
-};
-export const mode = 'development';
-export const devServer = {
-  static: join(__dirname, 'public'),
-  port: 3000,
-  open: true // Automatically open the browser
-};
-export const module = {
-  rules: [
-    {
-      test: /\.tsx?$/,
-      use: 'babel-loader',
-      exclude: /node_modules/
-    }
-  ]
-};
-export const resolve = {
-  extensions: ['.tsx', '.ts', '.js', '.jsx']
+// Manually define __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: _resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devServer: {
+    static: join(__dirname, 'public'),
+    port: 3000,
+    open: true, // Automatically open the browser
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
 };
