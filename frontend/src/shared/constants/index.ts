e701f72cf3168d85f837f46825a03fe7c3a6dff6
joshuaiokua <jiokua.albano@gmail.com>
2024-09-29 02:28:23 -0700

    Create index.ts

---
+++ frontend/src/shared/constants/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Frontend Constants
+ * @module frontend/src/shared/constants
+ * @description This module holds all the constants used across the application's frontend.
+ */
+
+import process from "process";
+
+export const baseURL = process.env.BACKEND_URL || "http://localhost:3001";
