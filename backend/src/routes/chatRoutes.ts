d1c7fb3c6cc7b945c2f6aea7aee9796cdc88e3fa
joshuaiokua <jiokua.albano@gmail.com>
2024-09-28 21:11:38 -0700

    feat: streamable message response

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -4,6 +4,7 @@ import { HumanMessage } from "@langchain/core/messages";
 
 // Internal Libraries
 import { createSimpleChatbot } from "../services/llm/chatService";
+import { getChatResponse } from "../services/llm/common";
 
 // Initialize router and chatbot
 const router = Router();
@@ -19,16 +20,25 @@ router.post("/", async (req: Request, res: Response): Promise<void> => {
       return;
     }
 
-    // Run the chatbot
-    const conversation = await chatbot.invoke(
+    // Construct events stream
+    const stream = chatbot.streamEvents(
       { messages: [new HumanMessage(message)] },
-      { configurable: { thread_id: "123" } } // TODO: programmatically generate thread_id
+      {
+        configurable: { thread_id: "123" },
+        version: "v2",
+      }
     );
 
-    // Get the assistant's reply
-    const lastResponse = conversation.messages.at(-1).content;
+    // Stream messages and collect message content chunks
+    let responseContent: string | undefined = "";
+    for await (const { event, data } of stream) {
+      if (event === "on_chat_model_stream") {
+        // console.log("Content:", data.chunk.content);
+        responseContent += data.chunk.content;
+      }
+    }
 
-    res.json({ reply: lastResponse });
+    res.json({ reply: responseContent });
   } catch (error) {
     console.error("Error in chat route:", error);
     res.status(500).json({ error: "Internal server error" });
