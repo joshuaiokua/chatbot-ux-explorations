ef8f8473cc7ef171adede278977cbe942dc2a604
joshuaiokua <jiokua.albano@gmail.com>
2024-09-30 02:06:40 -0700

    feat: style, css, sass, loaders
    Added requirements to use sass styling in project

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,19 +1,19 @@
-import { resolve as _resolve, join, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { resolve as _resolve, join, dirname } from "path";
+import { fileURLToPath } from "url";
 
 // Manually define __dirname in ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   output: {
-    filename: 'bundle.js',
-    path: _resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: _resolve(__dirname, "dist"),
   },
-  mode: 'development',
+  mode: "development",
   devServer: {
-    static: join(__dirname, 'public'),
+    static: join(__dirname, "public"),
     port: 3000,
     open: false, // Automatically open the browser if `true`
     hot: true, // Enable hot module replacement feature
@@ -22,12 +22,16 @@ export default {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
 };
